976c65b38ef0b926252721a04d92adcc24d8098c
Angel <53478143+007sya@users.noreply.github.com>
2021-04-14 10:22:42 +0200

    Initial commit

---
+++ src/PythonLib/lib/utils/secrets/secrets_template.py
@@ -0,0 +1,7 @@
+
+"""
+This is the template for the secrets.py file
+secrets.py file contains sensible information such as
+private API tokens, paswords, etc...
+"""
+TRAIN_DATA_PASSWORD = "asdasdasdasd"
